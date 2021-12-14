e52258c4795b040d5ff37b71c60e56d06ff3a1cb
wonsss <bigdatajang@gmail.com>
2021-12-15 00:03:39 +0900

    feat: Make Slider complete

--- 18_backgroundSlider/script.js
+++ 18_backgroundSlider/script.js
@@ -5,6 +5,28 @@ const rightBtn = document.getElementById('right');
 
 let activeSlide = 0;
 
+rightBtn.addEventListener('click', () => {
+  activeSlide++;
+
+  if (activeSlide > slides.length - 1) {
+    activeSlide = 0;
+  }
+
+  setBgToBody();
+  setActiveSlide();
+});
+
+leftBtn.addEventListener('click', () => {
+  activeSlide--;
+
+  if (activeSlide < 0) {
+    activeSlide = slides.length - 1;
+  }
+
+  setBgToBody();
+  setActiveSlide();
+});
+
 setBgToBody();
 
 function setBgToBody() {
